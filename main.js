97edeacaa255aab470a3d732e60305060a4e705d
loomedai <bbrith@hotmail.com>
2022-01-04 10:25:53 +0100

    syntax

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-const parallax = document.getElementById("kas1")
+const parallax = document.getElementById("kas1");
+
+window.addEventListener(("scroll", fucntion (e)){
+    let offset = window.pageYOffset;
+    parallax.style.backgroundPositionY = offset * 0.7 + "px";
+    console.log('Offset: ' + offset);
+    console.log('Offset: * 0.7' + offset * 0.7);
+
+});
+

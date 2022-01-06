98b2cdec7a3376387751d7e7c3aaa6fa3591af12
loomedai <bbrith@hotmail.com>
2022-01-06 14:32:23 +0100

    primitiv animation forside

--- js/main.js
+++ js/main.js
@@ -6,7 +6,15 @@
     });
 });
 
+function moveKasse(){
+    console.log("test")
+    let frKas=document.getElementById("frKas")
+    frKas.style.marginTop="80vh"
 
+    let first=document.getElementById("first")
+    first.style.marginTop="20vh"
+
+}
 
 
     $('.navbar a').on('click', function (e)

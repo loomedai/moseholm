b5adf46ae1b831d24b281f4c72f88cec19a0801b
loomedai <bbrith@hotmail.com>
2022-01-10 17:50:56 +0100

    gammel kode slettet

--- js/main.js
+++ js/main.js
@@ -9,36 +9,6 @@ $(function ()
     });
 });
 
-function moveKasse(){
-    let frKas=document.getElementById("frKas")
-    let frKas2=document.getElementById("meanBoy")
-
-    if (frKas.style.display=="none"){
-        frKas.style.display="block"
-    }
-
-    else {
-        frKas.style.display="none"
-
-    }
-
-    if (frKas2.style.display=="none"){
-        frKas2.style.display="block"
-    }
-
-    else {
-        frKas2.style.display="none"
-
-    }
-
-    // console.log("test")
-    // let frKas=document.getElementById("frKas")
-    // frKas.style.marginTop="80vh"
-    //
-    // let first=document.getElementById("first")
-    // first.style.marginTop="20vh"
-
-}
 
     let actionMenu = document.getElementById( "action-menu" );
     let isOpen = false;
